5738a363f6cf8614b4f6d282f6a93cb0891ced4d
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-06 22:32:42 +0300

    generate an api key

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     '''
     General Configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
 
 class ProdConfig(Config):
     '''
