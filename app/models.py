8ba4d8c76318645e255bdef7c12e0a200716dc29
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-09 23:15:19 +0300

    create a models.py and addd text to it

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Source:
+    '''
+    Source class to define source objects
+    '''
+
+    def __init__(self,id,name,description,language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.language = language
+
+class Article:
+    '''
+    Class Article to define article objects 
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+
+    # def save_article(self):
+    # Article.append(self)
